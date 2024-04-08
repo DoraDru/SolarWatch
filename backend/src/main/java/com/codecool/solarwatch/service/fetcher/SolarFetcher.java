c409df353099abd1fe458c9c9bd4370fbe3b13ec
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-04-08 13:16:36 +0200

    create SolarFetcher to get data from sunrise-sunset.org

---
+++ backend/src/main/java/com/codecool/solarwatch/service/fetcher/SolarFetcher.java
@@ -0,0 +1,57 @@
+package com.codecool.solarwatch.service.fetcher;
+
+import com.codecool.solarwatch.model.City;
+import com.codecool.solarwatch.model.solar.SolarResult;
+import com.codecool.solarwatch.model.solar.SunriseSunsetInfo;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class SolarFetcher {
+    private final WebClient webClient;
+
+    public SolarFetcher(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public SunriseSunsetInfo fetchSolarInfo(City city, LocalDate date) {
+        String url = getURL(date, city.getLatitude(), city.getLongitude());
+
+        SolarResult response = webClient
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(SolarResult.class)
+                .block();
+
+        if (response != null) {
+            SunriseSunsetInfo sunriseSunsetInfo = new SunriseSunsetInfo(
+                    date,
+                    convertToLocalTime(response.results().sunrise()),
+                    convertToLocalTime(response.results().sunset())
+            );
+            return sunriseSunsetInfo;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private String getURL(LocalDate date, double latitude, double longitude) {
+        return String.format("https://api.sunrise-sunset.org/json?lat=%s&lng=%s&date=%s", latitude, longitude, date);
+    }
+
+    private LocalTime convertToLocalTime(String time) {
+        int WRONG_TIME_FORMAT_LENGTH = 10;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+
+        if (time.length() == WRONG_TIME_FORMAT_LENGTH) {
+            String correctTimeFormat = "0" + time;
+            return LocalTime.parse(correctTimeFormat, formatter);
+        }
+        return LocalTime.parse(time, formatter);
+    }
+}
