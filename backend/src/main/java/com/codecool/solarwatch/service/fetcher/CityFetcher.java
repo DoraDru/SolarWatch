914351ee86dfad9f667563806713427009b3d1c4
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-04-08 12:00:56 +0200

    add CityFetcher to get data from openweathermap

---
+++ backend/src/main/java/com/codecool/solarwatch/service/fetcher/CityFetcher.java
@@ -0,0 +1,44 @@
+package com.codecool.solarwatch.service.fetcher;
+
+import com.codecool.solarwatch.errorhandling.InvalidCityException;
+import com.codecool.solarwatch.model.City;
+import com.codecool.solarwatch.model.GeoCoordinates;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class CityFetcher {
+    @Value("${api.key}")
+    private String API_KEY;
+    private final WebClient webClient;
+
+    public CityFetcher(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public City getGeoCodingCoordinates(String cityName) {
+
+        String url = String.format("https://api.openweathermap.org/geo/1.0/direct?q=%s&appid=%s", cityName, API_KEY);
+
+        GeoCoordinates[] response = webClient
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(GeoCoordinates[].class)
+                .block();
+
+        if (response != null && response.length > 0) {
+            GeoCoordinates geoCodingCoordinates = response[0];
+
+            return new City(
+                    geoCodingCoordinates.name(),
+                    geoCodingCoordinates.lat(),
+                    geoCodingCoordinates.lon(),
+                    geoCodingCoordinates.country(),
+                    geoCodingCoordinates.state());
+        } else {
+            throw new InvalidCityException();
+        }
+    }
+}
