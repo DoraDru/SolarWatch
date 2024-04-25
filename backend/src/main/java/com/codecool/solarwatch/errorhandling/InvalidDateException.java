e36dcdb7c01c6adb530cecb71527ece3b837f0cd
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-04-25 11:57:27 +0200

    change InvalidDateException message

--- backend/src/main/java/com/codecool/solarwatch/errorhandling/InvalidDateException.java
+++ backend/src/main/java/com/codecool/solarwatch/errorhandling/InvalidDateException.java
@@ -2,7 +2,6 @@ package com.codecool.solarwatch.errorhandling;
 
 public class InvalidDateException extends RuntimeException{
     public InvalidDateException(String date) {
-        super(String.format("Invalid date: %s \n" +
-                "Date format must be YYYY-MM-DD", date));
+        super(String.format("Invalid date: %s. Date format must be YYYY-MM-DD", date));
     }
 }
