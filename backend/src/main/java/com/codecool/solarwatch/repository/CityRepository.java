aad2466d937d7c7a3ad815ef8d2772538345e0db
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-05-06 11:57:51 +0200

    add missing @Repository annotations

--- backend/src/main/java/com/codecool/solarwatch/repository/CityRepository.java
+++ backend/src/main/java/com/codecool/solarwatch/repository/CityRepository.java
@@ -2,9 +2,11 @@ package com.codecool.solarwatch.repository;
 
 import com.codecool.solarwatch.model.city.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
     Optional<City> getCityByName(String name);
 }
--- backend/src/main/java/com/codecool/solarwatch/repository/SolarRepository.java
+++ backend/src/main/java/com/codecool/solarwatch/repository/SolarRepository.java
@@ -3,10 +3,12 @@ package com.codecool.solarwatch.repository;
 import com.codecool.solarwatch.model.city.City;
 import com.codecool.solarwatch.model.solar.SunriseSunsetInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
+@Repository
 public interface SolarRepository extends JpaRepository<SunriseSunsetInfo, Long> {
     Optional<SunriseSunsetInfo> getByCityAndDate(City city, LocalDate date);
 }
